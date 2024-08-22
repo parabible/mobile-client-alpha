87e7d4a808065226ba5aa7ee027daaced093b80d
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2024-08-22 00:01:41 -0500

    Make pwa

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
       include: ["**/*.res.mjs"],
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
     }),
   ],
 });
